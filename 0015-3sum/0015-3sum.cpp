c4288453cc2647b7ce7f614df8af1ec1e2ef6a78
Agnibha Sarkar <77271895+casafurix@users.noreply.github.com>
2022-12-03 22:22:45 +0530

    Time: 196 ms (44.23%), Space: 19.8 MB (85.46%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -5,19 +5,26 @@ public:
         
         int n = nums.size();
         
+        int target = 0;
+        
         vector<vector<int>> ans;
         
         for(int i = 0; i<n; i++){
             int a = nums[i];
-            int rem = -a;
             
+            // solving 3sum using 2sum approach
+            int rem = target - a;
+            
+            // 2 pointer-approach -> start, end
             int start = i + 1, end = n - 1;
             
             while(start < end){
                 if(nums[start] + nums[end] == rem){
                     ans.push_back({nums[i], nums[start], nums[end]});
+                    // to avoid repeating elements
                     while(start < end && nums[start] == nums[start+1]) start++;
                     while(start < end && nums[end] == nums[end-1]) end--;
+                    // shifting start and end pointers
                     start++;
                     end--;
                 } 
